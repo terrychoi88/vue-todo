1880a006bdb60ce200e1c13a08c5df8e1ec309c9
Terry <terrychoi88@users.noreply.github.com>
2021-02-24 21:42:40 +0900

    created

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+      <todo-header></todo-header>
+      <todo-input></todo-input>
+      <todo-list></todo-list>
+      <todo-footer></todo-footer>
+    </div>
+</template>
+
+<script>
+import TodoFooterVue from './components/TodoFooter.vue'
+import TodoHeaderVue from './components/TodoHeader.vue'
+import TodoInputVue from './components/TodoInput.vue'
+import TodoListVue from './components/TodoList.vue'
+
+export default {
+    components: {
+      'TodoHeader' : TodoHeaderVue,
+      'TodoInput' : TodoInputVue,
+      'TodoList' : TodoListVue,
+      'TodoFooter' : TodoFooterVue
+    }
+}
+</script>
+
+<style>
+
+</style>
