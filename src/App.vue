fcf16088ef62478092326e1938278026c18bd879
Terry <terrychoi88@users.noreply.github.com>
2021-02-25 21:47:49 +0900

    complete the props function

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="app">
       <todo-header></todo-header>
-      <todo-input></todo-input>
-      <todo-list></todo-list>
-      <todo-footer></todo-footer>
+      <todo-input @addTodo="addTodo"></todo-input>
+      <todo-list v-bind:propsdata="todoItems" v-on:removeTodo="removeTodo"></todo-list>
+      <todo-footer v-on:removeAll="clearAll"></todo-footer>
     </div>
 </template>
 
@@ -14,6 +14,32 @@ import TodoInputVue from './components/TodoInput.vue'
 import TodoListVue from './components/TodoList.vue'
 
 export default {
+    data() {
+        return {
+            todoItems: []
+        }
+    },
+   created() {
+        if (localStorage.length > 0) {
+            for (var i=0; i<localStorage.length; i++) {
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    },
+    methods: {
+        addTodo(todoItem) {
+            localStorage.setItem(todoItem, todoItem);
+            this.todoItems.push(todoItem);
+        },
+        clearAll() {
+            localStorage.clear();
+            this.todoItems = [];
+        },
+        removeTodo(t, i) {
+            localStorage.removeItem(t);
+            this.todoItems.splice(i,1);
+        }
+    },
     components: {
       'TodoHeader' : TodoHeaderVue,
       'TodoInput' : TodoInputVue,
@@ -24,5 +50,18 @@ export default {
 </script>
 
 <style>
-
+ body {
+        text-align: center;
+        background-color: #f6f6f8;
+    }
+    input {
+        border-style: groove;
+        width: 200px;
+    }
+    button {
+        border-style: groove;
+    }
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0, 0, 0.03);
+    }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,59 @@
 <template>
-    <div>list</div>
+    <section>
+        <transition-group name="list" tag="ul">
+            <li :key="index" :value="todoItem" v-for="(todoItem,index) in propsdata" class="shadow">
+                <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                {{todoItem}}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
+        </transition-group>
+    </section>
 </template>
 
 <script>
 export default {
-    
+    props: ['propsdata'],
+    methods: {
+        removeTodo(t, i) {
+            this.$emit('removeTodo', t, i);
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+    ul {
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .checkBtn {
+        line-height: 47px;
+        color: green;
+        margin-right: 6px;
+    }
+    .removeBtn {
+        margin-left: auto;
+        color: #de4343;
+    }
+    .list-enter-active, .list-leave-active {
+        transition: all 1s;
+    }
+    .list-enter, .list-leave {
+        opacity: 0;
+        transform: translateY(30px);
+    }
 </style>
